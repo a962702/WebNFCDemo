3078bd00f2c58dd7fc2623dead70ff8b5f76d7f0
Andy Yang <23339548+a962702@users.noreply.github.com>
2023-04-17 00:02:34 +0800

    upload

---
+++ index.js
@@ -0,0 +1,32 @@
+document.getElementById('first2second').addEventListener('click', ()=>{
+    document.getElementById('first_step').style.display = 'none';
+    document.getElementById('second_step').style.display = 'block';
+})
+
+document.getElementById('second2third').addEventListener('click', async ()=>{
+    document.getElementById('second_step').style.display = 'none';
+    document.getElementById('third_step').style.display = 'block';
+    // NFC
+    try {
+        const ndef = new NDEFReader();
+        await ndef.scan();
+
+        document.getElementById('third_step_status').innerText = "請開啟手機NFC功能，並將卡片放置於NFC感應區";
+    
+        ndef.addEventListener("readingerror", () => {
+            document.getElementById('third_step_status').innerText = "讀取時發生錯誤，請移開卡片，然後再試一次";
+        });
+    
+        ndef.addEventListener("reading", ({ message, serialNumber }) => {
+            document.getElementById('third_step_status').innerHTML = `已成功讀取卡片序號：${serialNumber}<br/>請按送出以完成卡片註冊`;
+        });
+      } catch (error) {
+            document.getElementById('third_step_status').innerText = "初始化 NDEFReader 元件發生無法復原之錯誤，可能是您的瀏覽器或裝置並不支援WebNFC API，請使用其他瀏覽器或裝置並再試一次";
+            document.getElementById('third2final').style.display = 'none';
+      }
+})
+
+document.getElementById('third2final').addEventListener('click', ()=>{
+    document.getElementById('third_step').style.display = 'none';
+    document.getElementById('final_step').style.display = 'block';
+})
